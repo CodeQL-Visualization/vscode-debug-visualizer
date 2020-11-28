d69a8cb4717dff44b9d6b7a457aaaf35d5249bb8
Robert MacGillivray <macgillivray.robert@gmail.com>
2020-11-28 03:20:01 -0700

    Update CodeQLService.ts
    - iteratively unpacking my data structure (+ a test)

--- extension/src/EvaluationWatchService/CodeQLService.ts
+++ extension/src/EvaluationWatchService/CodeQLService.ts
@@ -1,41 +1,49 @@
 import {
-	EvaluationWatchService,
-	EvaluationWatcher,
-	EvaluationWatcherOptions,
+   EvaluationWatchService,
+   EvaluationWatcher,
+   EvaluationWatcherOptions,
 } from "./EvaluationWatchService";
 import { observable, autorun, action } from "mobx";
 import { Disposable } from "@hediet/std/disposable";
 import { DataExtractorId, GraphNode, GraphVisualizationData } from "@hediet/debug-visualizer-data-extraction";
 import { DataExtractionState, CompletionItem } from "../webviewContract";
 import { hotClass } from "@hediet/node-reload";
+//import { TaintVisNode } from "../../../../visualization-helpers/vis-helpers";
+
+type TaintVisNode = {
+    uniqueID: string;
+    label: string;
+    tainted: boolean;
+    children: TaintVisNode[];
+}
 
 @hotClass(module)
 export class CodeQLWatchService implements EvaluationWatchService {
-	public readonly dispose = Disposable.fn();
-	private watcher : CodeQLWatcher;
+   public readonly dispose = Disposable.fn();
+   private watcher : CodeQLWatcher;
 
-	constructor() {
+   constructor() {
         this.watcher = new CodeQLWatcher('', this, {preferredDataExtractor: undefined});
-		this.dispose.track({
-			dispose: autorun(() => {
-				this.watcher.refresh();
-			}),
-		});
-	}
+      this.dispose.track({
+         dispose: autorun(() => {
+            this.watcher.refresh();
+         }),
+      });
+   }
 
-	public createEvaluationWatcher(
-		expression: string,
-		options: EvaluationWatcherOptions
-	): EvaluationWatcher {
-		this.refresh(this.watcher);
-		return this.watcher;
+   public createEvaluationWatcher(
+      expression: string,
+      options: EvaluationWatcherOptions
+   ): EvaluationWatcher {
+      this.refresh(this.watcher);
+      return this.watcher;
     }
     
     public createCodeQLGraph() {
         const graph : GraphVisualizationData = {
-			kind: { graph: true },
-			nodes: [],
-			edges: []
+         kind: { graph: true },
+         nodes: [],
+         edges: []
         };
  
         const result = {
@@ -49,78 +57,122 @@ export class CodeQLWatchService implements EvaluationWatchService {
                 },
                 data: graph,
             },
-        } as DataExtractionState;
+      } as DataExtractionState;
+
+      const root:TaintVisNode = {
+         uniqueID: "1",
+         label: "foo()",
+         tainted: true,
+         children: []
+      }
+
+      root.children.push({
+         uniqueID: "2",
+         label: "bar()",
+         tainted: false,
+         children: []
+      })
+
+      root.children.push({
+         uniqueID: "3",
+         label: "bar2()",
+         tainted: false,
+         children: []
+      })
+
+      const foo2: TaintVisNode = {
+         uniqueID: "4",
+         label: "foo2()",
+         tainted: true,
+         children: []
+      }
+
+      root.children[0].children.push(foo2);
+      root.children[1].children.push(foo2);
+      
+      let open: TaintVisNode[] = [root];
+      let gnRoot: GraphNode = {
+         id:root.uniqueID,
+         label:root.label,
+         color:(root.tainted ? "red" : "lightblue"),
+         shape:"box"
+      }
+      graph.nodes.push(gnRoot);
+      while (open.length > 0) {      
+         let current = open.pop();
+         if (current?.children != null && current.children.length > 0) {
+            for (let i = 0; i < current?.children.length; i++) {
+               open.push(current.children[i]);
+               let newNode: GraphNode = {
+                  id:current.children[i].uniqueID,
+                  label:current.children[i].label,
+                  color:(current.children[i].tainted ? "red" : "lightblue"),
+                  shape:"box"
+               }
+               graph.nodes.push(newNode);
+               let newLabel = "";
+               if (current.tainted && current.children[i].tainted) {
+                  newLabel = "taint";
+               } else if (current.tainted && !current.children[i].tainted) {
+                  newLabel = "sanitized";
+               } else if (!current.tainted && current.children[i].tainted) {
+                  newLabel = "spontaneous taint";
+               }
+               graph.edges.push({ from: current.uniqueID, to: current.children[i].uniqueID, label:newLabel});
+            }
+         }
+      }
         
-        const node1: GraphNode = {
-            id: "1",
-            label: "foo1()",
-            color:  "red",
-            shape: "box",
-        };
-
-        graph.nodes.push(node1);
-
-        const node2: GraphNode = {
-            id: "2",
-            label: "foo2()",
-            color:  "lightblue",
-            shape: "box",
-        };
-
-        graph.nodes.push(node2);
-
-        graph.edges.push({ from: "1", to: "2", label: "taint" });
-        
-		this.watcher._state = result;
+      this.watcher._state = result;
     }
 
-	get languageId(): string | undefined {
-		return undefined;
-	}
+   get languageId(): string | undefined {
+      return undefined;
+   }
 
-	public async refresh(w: CodeQLWatcher): Promise<void> {
-	}
+   public async refresh(w: CodeQLWatcher): Promise<void> {
+   }
 
-	public async getCompletions(
-		text: string,
-		column: number
-	): Promise<CompletionItem[]> {
+   public async getCompletions(
+      text: string,
+      column: number
+   ): Promise<CompletionItem[]> {
         return [];
-	}
+   }
 }
 
 class CodeQLWatcher implements EvaluationWatcher {
-	constructor(
-		public readonly expression: string,
-		private readonly source: CodeQLWatchService,
-		options: EvaluationWatcherOptions
-	) {
-		this._preferredDataExtractor = options.preferredDataExtractor;
-	}
+   constructor(
+      public readonly expression: string,
+      private readonly source: CodeQLWatchService,
+      options: EvaluationWatcherOptions
+   ) {
+      this._preferredDataExtractor = options.preferredDataExtractor;
+   }
 
-	@observable
-	private _preferredDataExtractor: DataExtractorId | undefined = undefined;
+   @observable
+   private _preferredDataExtractor: DataExtractorId | undefined = undefined;
 
-	public get preferredDataExtractor(): DataExtractorId | undefined {
-		return this._preferredDataExtractor;
-	}
+   public get preferredDataExtractor(): DataExtractorId | undefined {
+      return this._preferredDataExtractor;
+   }
 
-	@action
-	public setPreferredDataExtractor(id: DataExtractorId | undefined): void {
-		this._preferredDataExtractor = id;
-		this.refresh();
-	}
+   @action
+   public setPreferredDataExtractor(id: DataExtractorId | undefined): void {
+      this._preferredDataExtractor = id;
+      this.refresh();
+   }
 
-	public refresh(): void {
-		this.source.refresh(this);
-	}
+   public refresh(): void {
+      this.source.refresh(this);
+   }
 
-	@observable
-	public _state: DataExtractionState = { kind: "error", message:"Unexpected error" };
-	public get state(): DataExtractionState {
-		return this._state;
-	}
+   @observable
+   public _state: DataExtractionState = { kind: "error", message:"Unexpected error" };
+   public get state(): DataExtractionState {
+      return this._state;
+   }
 
-	public dispose(): void {
-	}
+   public dispose(): void {
+   }
 }
